bd27b10fe425699df3acd88bf8ef7adddbe3ee77
tariqul12 <mshimul591@gmail.com>
2024-04-24 17:58:39 +0600

    commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 
-function searchInsertPosition($nums, $target) {
+function searchPosition($nums, $target) {
     $length = count($nums);
     for ($i = 0; $i < $length; $i++) {
         if ($nums[$i] >= $target) {
@@ -10,9 +10,9 @@ function searchInsertPosition($nums, $target) {
     return $length;
 }
 
-// Example usage:
+
 $nums = [1, 3, 2, 6];
 $target = 7;
-echo searchInsertPosition($nums, $target);
+echo searchPosition($nums, $target);
 
 ?>
